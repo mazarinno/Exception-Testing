dcfb18eb9fd642dc090b28ec1e58bb26276a3be8
mazarinno <mcsfakianos@gmail.com>
2023-03-26 03:37:49 -0400

    Update Exceptions.cpp
    fixed throwing standard exception and added custom exception

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -2,39 +2,47 @@
 //
 
 #include <iostream>
-#include <functional>
+
+// custom exception
+class myexception : public std::exception
+{
+    public:
+        virtual const char* what() const throw()
+        {
+            return "My Custom CPP Exception Thrown";
+        }
+};
 
 bool do_even_more_custom_application_logic()
 {
-  // TODO: Throw any standard exception
-  // bad function call exception by calling null function
+  // TODO: Throw any standard exception , done
 
-  std::function<int()> f = nullptr;
-  try {
-      f();
-  }
-  catch (const std::bad_function_call& e) {
-      std::cout << e.what() << '\n';
-  }
+  throw std::invalid_argument("throwing standard exception");
 
   std::cout << "Running Even More Custom Application Logic." << std::endl;
 
   return true;
 }
+
 void do_custom_application_logic()
 {
   // TODO: Wrap the call to do_even_more_custom_application_logic()
   //  with an exception handler that catches std::exception, displays
-  //  a message and the exception.what(), then continues processing
+  //  a message and the exception.what(), then continues processing. done
   std::cout << "Running Custom Application Logic." << std::endl;
 
-  if(do_even_more_custom_application_logic())
-  {
-    std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+  try {
+      do_even_more_custom_application_logic();
+  }
+  catch (const std::invalid_argument& e) {
+      std::cout << "Error: " << e.what() << '\n';
   }
 
   // TODO: Throw a custom exception derived from std::exception
   //  and catch it explictly in main
+
+  myexception e;
+  throw e;
 
   std::cout << "Leaving Custom Application Logic." << std::endl;
 
@@ -68,8 +76,17 @@ int main()
   //  std::exception
   //  uncaught exception 
   //  that wraps the whole main function, and displays a message to the console.
+  
+
   do_division();
-  do_custom_application_logic();
+
+  try {
+      do_custom_application_logic();
+  }
+  catch (myexception me) {
+      std::cout << "Error: " << me.what() << '\n';
+  }
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
